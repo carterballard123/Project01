b37afc8fbc6a882291daac37f1de530159fb3cdc
Carter Ballard <ballard.carter1@gmail.com>
2024-09-15 12:15:18 -0500

    some more variables - seeing if github works...

--- theGame.c
+++ theGame.c
@@ -4,9 +4,8 @@ int main(){
 
 int i;
 int ans1, ans2, ans3, ans4, ans5;
-int p1, p2, p3, p4, p5, p6, p7, p8, p9, p10
-
-
+int p1, p2, p3, p4, p5, p6, p7, p8, p9, p10;
+int p01, p02, p03, p04, p05, p06, p07, p08, p09, p010;
 
 return 0;
 }
